95ca839965170f58477f1449dc1bff58c373b0a7
wthkparn <151071664+wthkparn@users.noreply.github.com>
2024-05-13 10:41:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ const initmysql = async () => {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    // waitForConnections: true,
-   // connectionLimit: 10,
-   // queueLimit: 0
   });
 
 
